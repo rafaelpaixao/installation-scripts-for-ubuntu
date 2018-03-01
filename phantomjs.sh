eebbcac63b81c20441298b86689f838f8bad4394
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-01 09:42:19 -0300

    Update phantomjs

--- phantomjs.sh
+++ phantomjs.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 PHANTOM_VERSION=${1:-2.1.1}
+echo "--- Installation of Phantom JS $PHANTOM_VERSION ---"
 echo "Downloading..."
 sudo wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOM_VERSION-linux-x86_64.tar.bz2
 echo "Installing..."
@@ -6,4 +8,4 @@ sudo tar xvjf phantomjs-$PHANTOM_VERSION-linux-x86_64.tar.bz2 -C /usr/local/shar
 sudo ln -s /usr/local/share/phantomjs-$PHANTOM_VERSION-linux-x86_64/bin/phantomjs /usr/local/bin/
 echo "Cleaning..."
 sudo rm phantomjs-$PHANTOM_VERSION-linux-x86_64.tar.bz2
-echo "All done!"
+echo "--- All done! ---"
