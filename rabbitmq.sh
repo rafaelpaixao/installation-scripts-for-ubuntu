ff21fc9fcdc8b12560c5a685967cf30f1b544c9f
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-01 16:50:27 -0300

    hide output

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,9 +2,9 @@
 echo "--- Installation of RabbitMQ Server ---"
 echo "Add repo..."
 echo 'deb http://www.rabbitmq.com/debian/ testing main' |
-     sudo tee /etc/apt/sources.list.d/rabbitmq.list
+     sudo tee /etc/apt/sources.list.d/rabbitmq.list >/dev/null 2>&1
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |
-     sudo apt-key add -
+     sudo apt-key add - >/dev/null 2>&1
 echo "System update..."
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update;
 echo "Installing..."
