d294d91e6966a8e5f227159f3b1dd7adf99523ec
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-01 10:38:11 -0300

    Create composer.sh

---
+++ composer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "--- Installation of Composer ---"
+echo "Downloading..."
+curl -sS https://getcomposer.org/installer -o composer-setup.php > /dev/null 2>&1
+echo "Installing..."
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
+echo "Cleaning..."
+rm composer-setup.php
+echo "--- All done! ---"
