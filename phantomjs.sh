8bd372b713a3c1f60645a0e01583dceb4c7fa734
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-01 17:00:28 -0300

    fix filename

--- phantomjs.sh
+++ phantomjs.sh
@@ -3,7 +3,7 @@ PHANTOM_VERSION=${1:-2.1.1}
 FILENAME=phantomjs-$PHANTOM_VERSION-linux-x86_64.tar.bz2
 echo "--- Installation of Phantom JS $PHANTOM_VERSION ---"
 echo "Downloading..."
-sudo wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$FILENAME
+sudo wget -q https://bitbucket.org/ariya/phantomjs/downloads/$FILENAME
 echo "Installing..."
 sudo tar xvjf $FILENAME -C /usr/local/share/
 sudo ln -s /usr/local/share/$FILENAME/bin/phantomjs /usr/local/bin/
