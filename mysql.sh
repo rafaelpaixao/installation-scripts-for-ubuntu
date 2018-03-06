79c5eeea75f4b91d09076e6d633cf0c8af420e1f
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-05 22:40:44 -0300

    fix mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -8,11 +8,11 @@ MYSQL_ROOT_PASS=${4:-"root"}
 echo "--- Installation of MySQL Server ---"
 
 echo "Configuring..."
-debconf-set-selections <<< "mysql-server mysql-server/root_password password "$MYSQL_ROOT_PASS
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password "$MYSQL_ROOT_PASS
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password "$MYSQL_ROOT_PASS
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password "$MYSQL_ROOT_PASS
 echo "Installing..."
 sudo apt-get install -qq --fix-missing --allow-unauthenticated install mysql-server > /dev/null 2>&1
 echo "Creating default database..."
-mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE "$MYSQL_DB
-mysql -uroot -p$MYSQL_ROOT_PASS -e "grant all privileges on "$MYSQL_DB".* to '"$MYSQL_USER"'@'localhost' identified by '"$MYSQL_PASS"'"
+mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE "$MYSQL_DB > /dev/null 2>&1
+mysql -uroot -p$MYSQL_ROOT_PASS -e "grant all privileges on "$MYSQL_DB".* to '"$MYSQL_USER"'@'localhost' identified by '"$MYSQL_PASS"'" > /dev/null 2>&1
 echo "--- All done! ---"
