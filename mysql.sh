b86e3b02239db18bbaba72e86a267e2ce797165e
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-03-06 08:28:13 -0300

    fix mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ echo "Configuring..."
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password "$MYSQL_ROOT_PASS
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password "$MYSQL_ROOT_PASS
 echo "Installing..."
-sudo apt-get install -qq --fix-missing --allow-unauthenticated install mysql-server > /dev/null 2>&1
+sudo apt-get install -qq --fix-missing --allow-unauthenticated mysql-server > /dev/null 2>&1
 echo "Creating default database..."
 mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE "$MYSQL_DB > /dev/null 2>&1
 mysql -uroot -p$MYSQL_ROOT_PASS -e "grant all privileges on "$MYSQL_DB".* to '"$MYSQL_USER"'@'localhost' identified by '"$MYSQL_PASS"'" > /dev/null 2>&1
