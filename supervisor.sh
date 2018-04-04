a3e7169c3d9f37a5942021ccc8db3c2425b62047
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-04-04 09:54:29 -0300

    Add script for Supervisor

---
+++ supervisor.sh
@@ -0,0 +1,40 @@
+echo -e "\n--- Installation of Supervisor ---\n"
+
+install () {
+    echo "Installing $@..."
+    sudo apt-get install -qq --fix-missing --allow-unauthenticated $@ > /dev/null 2>&1
+}
+
+install supervisor
+
+echo "Applying some fixes..."
+
+sudo touch /var/run/supervisor.sock
+sudo chmod 777 /var/run/supervisor.sock
+
+sudo rm /etc/supervisor/supervisor.conf
+sudo -u root sh <<EOF
+sudo echo "
+[unix_http_server]
+file=/var/run/supervisor.sock
+chmod=0777
+
+[supervisord]
+logfile=/var/log/supervisor/supervisord.log
+pidfile=/var/run/supervisord.pid
+childlogdir=/var/log/supervisor
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl=unix:///var/run/supervisor.sock
+
+[include]
+files = /etc/supervisor/conf.d/*.conf
+" > /etc/supervisor/supervisor.conf
+EOF
+
+sudo -u root systemctl reload supervisor
+
+echo -e "\n--- All done! ---\n"
