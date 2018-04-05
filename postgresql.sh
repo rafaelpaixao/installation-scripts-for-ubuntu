cb69e23689c194fa6d95e5351e0bb10ee509c300
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-04-05 09:26:49 -0300

    Converted user, pass and db name to named params

--- postgresql.sh
+++ postgresql.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-POSTGRES_USER=${1:-"root"}
-POSTGRES_PASS=${2:-"root"}
-POSTGRES_DB=${3:-"example"}
+<<COMMENT
+    params:
+        --user       Username for new user, optional
+        --pass       Password for new user, if not provided will use the same value of pguser
+        --db         Name for new database, if not provided will use the same value of pguser
+    example:
+        bash postgresql.sh --user ubuntu
+COMMENT
 
-echo "--- Installation of PostgreSQL Server ---"
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+   fi
+  shift
+done
+
+echo -e "\n--- Installation of PostgreSQL Server ---\n"
 
 if ! [ -x "$(command -v psql)" ]; then 
 
 echo "Installing..."
 sudo apt-get install -qq --fix-missing --allow-unauthenticated postgresql > /dev/null 2>&1
 sudo apt-get install -qq --fix-missing --allow-unauthenticated postgresql-contrib > /dev/null 2>&1
-echo "Creating user and database..."
+
+if [ -z ${user+x} ]; then
+    echo "User not provided, skipping..."
+else
+    if [ -z ${pass+x} ]; then
+        echo "Pass not provided, using the same value of user..."
+        pass=$user
+    fi
+    if [ -z ${db+x} ]; then
+        echo "Database's name not provided, using the same value of user..."
+        db=$user
+    fi
+    echo "Creating user and database..."
 sudo -u postgres sh <<EOF
-    psql -c "CREATE USER $POSTGRES_USER PASSWORD '$POSTGRES_PASS'";
-    psql -c "CREATE DATABASE $POSTGRES_DB";
-    psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER";
-    psql -c "ALTER USER postgres PASSWORD 'postgres'";
+psql -c "CREATE USER $user PASSWORD '$pass'";
+psql -c "CREATE DATABASE $db";
+psql -c "GRANT ALL PRIVILEGES ON DATABASE $db TO $user";
+psql -c "ALTER USER postgres PASSWORD 'postgres'";
 EOF
-echo "--- All done! ---"
+fi
+echo -e "\n--- All done! ---\n"
 
 else
     echo "Postgres is already installed!"
